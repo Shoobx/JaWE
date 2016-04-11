331107b0dbb715a1c1fc212758aa336ad4bc920c
Sasa Bojanic <sasaboy@together.at>
2016-04-11 12:34:11 +0000

    # Fix: when saving file which has "." in its name, extension was not appended to the end
    git-svn-id: svn://svn.code.sf.net/p/jawe/code/trunk@789 4bd7e58f-8986-49bf-85ab-427c71a3f9e1

--- twe/modules/Utilities/src/org/enhydra/jawe/JaWEXMLUtil.java
+++ twe/modules/Utilities/src/org/enhydra/jawe/JaWEXMLUtil.java
@@ -1,20 +1,20 @@
 /**
-* Together Workflow Editor
-* Copyright (C) 2011 Together Teamsolutions Co., Ltd. 
-* 
-* This program is free software: you can redistribute it and/or modify 
-* it under the terms of the GNU General Public License as published by 
-* the Free Software Foundation, either version 3 of the License, or 
-* (at your option) any later version. 
-*
-* This program is distributed in the hope that it will be useful, 
-* but WITHOUT ANY WARRANTY; without even the implied warranty of 
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
-* GNU General Public License for more details. 
-*
-* You should have received a copy of the GNU General Public License 
-* along with this program. If not, see http://www.gnu.org/licenses
-*/
+ * Together Workflow Editor
+ * Copyright (C) 2011 Together Teamsolutions Co., Ltd. 
+ * 
+ * This program is free software: you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation, either version 3 of the License, or 
+ * (at your option) any later version. 
+ *
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+ * GNU General Public License for more details. 
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see http://www.gnu.org/licenses
+ */
 
 package org.enhydra.jawe;
 
@@ -27,73 +27,78 @@ import javax.swing.JOptionPane;
 import org.enhydra.jawe.base.controller.JaWEController;
 
 /**
- * Class which purpose is to provide static methods which are
- * used by classes that represents program apstraction of
- * XML elements. These methods offers support for reading or
- * writting an XML document and for generating the tooltips for
- * for the classes that needs it.
+ * Class which purpose is to provide static methods which are used by classes that
+ * represents program apstraction of XML elements. These methods offers support for
+ * reading or writting an XML document and for generating the tooltips for for the classes
+ * that needs it.
  */
 public class JaWEXMLUtil {
 
    // filter for XML files
    private static JaWEXMLUtil.XMLFilter allFilter = new JaWEXMLUtil.XMLFilter(null);
+
    private static JaWEXMLUtil.XMLFilter xmlFilter = new JaWEXMLUtil.XMLFilter("xml");
+
    private static JaWEXMLUtil.XMLFilter xpdlFilter = new JaWEXMLUtil.XMLFilter("xpdl");
+
    private static JaWEXMLUtil.XMLFilter jpgFilter = new JaWEXMLUtil.XMLFilter("jpg");
+
    private static JaWEXMLUtil.XMLFilter svgFilter = new JaWEXMLUtil.XMLFilter("svg");
+
    private static JaWEXMLUtil.XMLFilter pdfFilter = new JaWEXMLUtil.XMLFilter("pdf");
+
    private static JaWEXMLUtil.XMLFilter txtFilter = new JaWEXMLUtil.XMLFilter("txt");
 
-   private static javax.swing.filechooser.FileFilter[] lastChoosenFilter=
-      new javax.swing.filechooser.FileFilter[6];
+   private static javax.swing.filechooser.FileFilter[] lastChoosenFilter = new javax.swing.filechooser.FileFilter[6];
    static {
-      lastChoosenFilter[0]=xpdlFilter;
-      lastChoosenFilter[1]=jpgFilter;
-      lastChoosenFilter[2]=svgFilter;
-      lastChoosenFilter[3]=pdfFilter;
-      lastChoosenFilter[4]=txtFilter;
-      lastChoosenFilter[5]=allFilter;
+      lastChoosenFilter[0] = xpdlFilter;
+      lastChoosenFilter[1] = jpgFilter;
+      lastChoosenFilter[2] = svgFilter;
+      lastChoosenFilter[3] = pdfFilter;
+      lastChoosenFilter[4] = txtFilter;
+      lastChoosenFilter[5] = allFilter;
    }
 
    private static JFileChooser chooser;
 
    static {
-      String userDir=System.getProperty("user.dir");
-      chooser=new JFileChooser(userDir);
+      String userDir = System.getProperty("user.dir");
+      chooser = new JFileChooser(userDir);
    }
 
    public static final int INFORMATION_MESSAGE = JOptionPane.INFORMATION_MESSAGE;
+
    public static final int ERROR_MESSAGE = JOptionPane.ERROR_MESSAGE;
 
-   //************************* XMLFILTER CLASS ***********************************
+   // ************************* XMLFILTER CLASS ***********************************
    /**
     * Used to filter XML files during opening or saving.
     */
    private static final class XMLFilter extends javax.swing.filechooser.FileFilter {
-      private String myExtension=null;
+      private String myExtension = null;
 
-      XMLFilter (String extension) {
-         this.myExtension=extension;
+      XMLFilter(String extension) {
+         this.myExtension = extension;
       }
 
-      public String getExtension () {
+      public String getExtension() {
          return myExtension;
       }
 
-      public boolean accept (File f) {
-         if(f != null) {
-            if(f.isDirectory()) {
+      public boolean accept(File f) {
+         if (f != null) {
+            if (f.isDirectory()) {
                return true;
             }
-            if (myExtension!=null) {
+            if (myExtension != null) {
                String extension = null;
                String fName = f.getName();
                int i = fName.lastIndexOf('.');
-               if(i>0 && i<fName.length()-1) {
-                  extension=fName.substring(i+1).toLowerCase();
+               if (i > 0 && i < fName.length() - 1) {
+                  extension = fName.substring(i + 1).toLowerCase();
                }
 
-               if(extension != null && extension.equalsIgnoreCase(myExtension)) {
+               if (extension != null && extension.equalsIgnoreCase(myExtension)) {
                   return true;
                }
             } else {
@@ -103,21 +108,21 @@ public class JaWEXMLUtil {
          return false;
       }
 
-      public String getDescription () {
-         JaWEController jc=JaWEManager.getInstance().getJaWEController();
-         if (myExtension!=null) {
-            return jc.getSettings().getLanguageDependentString(myExtension.toUpperCase()+"Description");
-         } 
-         return jc.getSettings().getLanguageDependentString("ALLDescription");         
+      public String getDescription() {
+         JaWEController jc = JaWEManager.getInstance().getJaWEController();
+         if (myExtension != null) {
+            return jc.getSettings().getLanguageDependentString(myExtension.toUpperCase() + "Description");
+         }
+         return jc.getSettings().getLanguageDependentString("ALLDescription");
       }
    }
-   //********************** END OF XMLFILTER CLASS ******************************
+
+   // ********************** END OF XMLFILTER CLASS ******************************
 
    /* Show a dialog with the given error message. */
-   public static void message(String message,int type) {
-      JaWEController jc=JaWEManager.getInstance().getJaWEController();
-      JOptionPane.showMessageDialog(jc.getJaWEFrame(),message,
-            jc.getSettings().getLanguageDependentString("DialogTitle"),type);
+   public static void message(String message, int type) {
+      JaWEController jc = JaWEManager.getInstance().getJaWEController();
+      JOptionPane.showMessageDialog(jc.getJaWEFrame(), message, jc.getSettings().getLanguageDependentString("DialogTitle"), type);
    }
 
    /* Open a dialog and return the filename. Returns null if canceled.
@@ -130,8 +135,7 @@ public class JaWEXMLUtil {
     * @param initialName The initial name of the file to be saved or opened
     * @return The filename of opened/saved file.
     */
-   public static String dialog(Component parent,String message,
-                               int mode,int filteringMode,String initialName) {
+   public static String dialog(Component parent, String message, int mode, int filteringMode, String initialName) {
       chooser.updateUI();
       chooser.setDialogTitle(message);
       int setFilterIndex = 0;
@@ -170,16 +174,16 @@ public class JaWEXMLUtil {
             setFilterIndex = 2;
             break;
          case 3:
-             chooser.setAcceptAllFileFilterUsed(false);
-             chooser.setFileFilter(allFilter);
-             chooser.setFileFilter(pdfFilter);
-             chooser.removeChoosableFileFilter(xmlFilter);
-             chooser.removeChoosableFileFilter(xpdlFilter);
-             chooser.removeChoosableFileFilter(jpgFilter);
-             chooser.removeChoosableFileFilter(svgFilter);
-             chooser.removeChoosableFileFilter(txtFilter);
-             setFilterIndex = 3;
-             break;
+            chooser.setAcceptAllFileFilterUsed(false);
+            chooser.setFileFilter(allFilter);
+            chooser.setFileFilter(pdfFilter);
+            chooser.removeChoosableFileFilter(xmlFilter);
+            chooser.removeChoosableFileFilter(xpdlFilter);
+            chooser.removeChoosableFileFilter(jpgFilter);
+            chooser.removeChoosableFileFilter(svgFilter);
+            chooser.removeChoosableFileFilter(txtFilter);
+            setFilterIndex = 3;
+            break;
          case 4:
             chooser.setAcceptAllFileFilterUsed(false);
             chooser.setFileFilter(allFilter);
@@ -205,71 +209,72 @@ public class JaWEXMLUtil {
       }
       try {
          chooser.setFileFilter(lastChoosenFilter[setFilterIndex]);
-      } catch (Exception ex) {}
+      } catch (Exception ex) {
+      }
 
-      if (initialName!=null && initialName.length()>0) {
-         File f=new File(initialName);
+      if (initialName != null && initialName.length() > 0) {
+         File f = new File(initialName);
          chooser.setSelectedFile(f);
          chooser.ensureFileIsVisible(f);
       }
 
-      int returnVal=-1;
-      String fileName=null;
+      int returnVal = -1;
+      String fileName = null;
 
       while (true) {
-         if (mode==0) {
+         if (mode == 0) {
             returnVal = chooser.showOpenDialog(parent);
-         }
-         else {
+         } else {
             returnVal = chooser.showSaveDialog(parent);
          }
 
-         if(returnVal == JFileChooser.APPROVE_OPTION) {
-            File f=chooser.getSelectedFile();
-            fileName=f.getAbsolutePath();
+         if (returnVal == JFileChooser.APPROVE_OPTION) {
+            File f = chooser.getSelectedFile();
+            fileName = f.getAbsolutePath();
             // SAVING:
-            // - if extension isn't specified, tries to save the file with default extension
-            // - it will not save file with extension if the file with extension already exists
-            String extension=((JaWEXMLUtil.XMLFilter)chooser.getFileFilter()).getExtension();
-            int dotIndex=f.getName().lastIndexOf(".");
+            // - if extension isn't specified, tries to save the file with default
+            // extension
+            // - it will not save file with extension if the file with extension already
+            // exists
+            String extension = ((JaWEXMLUtil.XMLFilter) chooser.getFileFilter()).getExtension();
+            int dotIndex = f.getName().lastIndexOf(".");
 
-            String oldFilename=fileName;
-            JaWEController jc=JaWEManager.getInstance().getJaWEController();
-            if (mode==1) {
-               if ((filteringMode>=0 && filteringMode<=3) && extension!=null &&
-                   dotIndex==-1) {
-                  fileName+="."+extension;
+            String oldFilename = fileName;
+            JaWEController jc = JaWEManager.getInstance().getJaWEController();
+            if (mode == 1) {
+               if ((filteringMode >= 0 && filteringMode <= 3) && extension != null) {
+                  fileName += "." + extension;
                }
                // check if user have choosed an existing filename
                if (new File(fileName).exists()) {
-                  int r=JOptionPane.showConfirmDialog(jc.getJaWEFrame(),
-                        jc.getSettings().getLanguageDependentString("WarningFileAlreadyExistsDoYouWantToReplaceIt"),
-                        jc.getSettings().getLanguageDependentString("DialogTitle"),JOptionPane.YES_NO_OPTION);
-                  if (r==JOptionPane.NO_OPTION) {
-                     fileName=oldFilename;
+                  int r = JOptionPane.showConfirmDialog(jc.getJaWEFrame(),
+                                                        jc.getSettings().getLanguageDependentString("WarningFileAlreadyExistsDoYouWantToReplaceIt"),
+                                                        jc.getSettings().getLanguageDependentString("DialogTitle"),
+                                                        JOptionPane.YES_NO_OPTION);
+                  if (r == JOptionPane.NO_OPTION) {
+                     fileName = oldFilename;
                      continue;
                   }
                }
 
                // OPENING:
                // - if extension isn't specified, and given file doesn't exist, tries
-               //   to open the .xml file with the same name
+               // to open the .xml file with the same name
             } else {
-               if (mode==0 && !f.exists()) {
-                  if ((filteringMode>=0 && filteringMode<=3) && dotIndex==-1) {
-                     fileName+="."+extension;
+               if (mode == 0 && !f.exists()) {
+                  if ((filteringMode >= 0 && filteringMode <= 3) && dotIndex == -1) {
+                     fileName += "." + extension;
                      if (!new File(fileName).exists()) {
-                        fileName=null;
+                        fileName = null;
                      }
                   } else {
-                     fileName=null;
+                     fileName = null;
                   }
                }
-               if (fileName==null) {
-                  JaWEXMLUtil.message(
-                        jc.getSettings().getLanguageDependentString("WarningFileDoesNotExistPleaseSelectExistingFileToOpen"),
-                     JOptionPane.WARNING_MESSAGE);
-                  fileName=oldFilename;
+               if (fileName == null) {
+                  JaWEXMLUtil.message(jc.getSettings().getLanguageDependentString("WarningFileDoesNotExistPleaseSelectExistingFileToOpen"),
+                                      JOptionPane.WARNING_MESSAGE);
+                  fileName = oldFilename;
                   continue;
                }
             }
@@ -278,11 +283,11 @@ public class JaWEXMLUtil {
       }
 
       try {
-         lastChoosenFilter[setFilterIndex]=chooser.getFileFilter();
-      } catch (Exception ex) {}
+         lastChoosenFilter[setFilterIndex] = chooser.getFileFilter();
+      } catch (Exception ex) {
+      }
 
       return fileName;
    }
 
 }
-
