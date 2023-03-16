defac0a2d5301e73aed4e89f14415d034ed5c7f2
Stephan Richter <stephan.richter@gmail.com>
2023-03-16 15:49:56 -0400

    Do not reset screen.

--- twe/configure.bat
+++ twe/configure.bat
@@ -14,7 +14,6 @@
 ::along with this program. If not, see http://www.gnu.org/licenses
 ::
 @echo off
-cls
 rem *********************************************
 rem *  Initialize environment variables
 rem *********************************************
